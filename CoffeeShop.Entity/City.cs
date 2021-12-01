3c12da0fb475ec75f5360d1824995a84d825e5d3
kerimeburcu@outlook.com <kerimeburcu@outlook.com>
2021-12-01 23:42:24 +0300

    city ve region sınıfları tanımlandı.

---
+++ CoffeeShop.Entity/City.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeShop.Entity
+{
+    public class City
+    {
+        //şehirler arasında uniqueliğin sağlanması için primarykey tanımlandı.
+        public int Id { get; set; }
+        public string CityName { get; set; }
+
+        //şehirlerin bölgeleri list olarak tanımlandı.
+        public List<Region> Regions { get; set; }
+
+
+    }
+}
---
+++ CoffeeShop.Entity/Region.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeShop.Entity
+{
+    public class Region
+    {
+       
+        public int Id { get; set; }
+        public string RegionName { get; set; }
+
+        //şehir ile bölgeler arasında foreinkey oluşturuldu.
+        public int CityId { get; set; }
+
+        public City City { get; set; }
+
+
+
+    }
+}
