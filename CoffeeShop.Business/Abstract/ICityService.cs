5055b2b713f4b6b1719d6005f43cb1ede32c2c53
kerimeburcu@outlook.com <kerimeburcu@outlook.com>
2021-12-04 01:39:28 +0300

    CityService Get ve GetAll eklendi.

---
+++ CoffeeShop.Business/Abstract/ICityService.cs
@@ -0,0 +1,15 @@
+﻿using CoffeeShop.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeShop.Business.Abstract
+{
+    public interface ICityService
+    {
+        IList<City> GetCities();
+        City GetCity(int id);
+    }
+}
---
+++ CoffeeShop.Business/Concrete/CityService.cs
@@ -0,0 +1,31 @@
+﻿using CoffeeShop.Business.Abstract;
+using CoffeeShop.DataAccess.Abstract;
+using CoffeeShop.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeShop.Business.Concrete
+{
+    public class CityService : ICityService
+    {
+
+        private readonly ICityRepository _cityRepository;
+        public CityService(ICityRepository cityRepository)
+        {
+            _cityRepository = cityRepository; 
+        }
+        public IList<City> GetCities()
+        {
+            return _cityRepository.GetAll().ToList();
+
+        }
+
+        public City GetCity(int id)
+        {
+            return _cityRepository.Get(q => q.Id == id);
+        }
+    }
+}
